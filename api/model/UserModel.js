68c0146401df1eca640249db0240d2f741641564
IndrajitParmar <parindra231@gmail.com>
2023-11-10 22:03:37 +0530

    model created

---
+++ api/model/UserModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.model(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timesStamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
